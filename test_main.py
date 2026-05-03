1a1e3e16d948db693083093005f18c25306955dd
Кузмина Анастасия <kuzminaanastasia635@gmail.com>
2026-05-03 14:05:32 +0300

    Fix flake8 styling errors

--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@
 # Імпортуємо наші функції з файлу main.py
 from main import count_words, is_text_alpha
 
+
 # Тест №1: Перевіряємо правильний підрахунок слів
 def test_count_words():
     # Очікуємо, що у фразі "Привіт світ" буде рівно 2 слова
@@ -10,9 +11,10 @@ def test_count_words():
     # Очікуємо, що порожній рядок поверне 0 слів
     assert count_words("") == 0
 
+
 # Тест №2: Перевіряємо аналіз тексту на літери
 def test_is_text_alpha():
     # Текст без цифр має повернути True
-    assert is_text_alpha("Тільки літери") == True
+    assert is_text_alpha("Тільки літери")
     # Текст із цифрами має повернути False
-    assert is_text_alpha("Літери та цифри 123") == False
+    assert not is_text_alpha("Літери та цифри 123")
